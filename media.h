a27ff4af582e4a1d400ae35a671c7a1803842fa1
lestandin <lestandi.nathan@gmail.com>
2017-04-04 14:32:11 +0200

    voila

---
+++ media.h
@@ -0,0 +1,9 @@
+#define PERSONNE_H
+
+
+#include <stdlib.h>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+
---
+++ mediatheque.cpp
@@ -0,0 +1,15 @@
+// mediatheque.cpp : définit le point d'entrée pour l'application console.
+//
+#include "stdafx.h"
+#include "Personne.h"
+#include <algorithm>
+#include <cassert>
+
+
+
+
+int main()
+{
+    return 0;
+}
+
